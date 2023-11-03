ab9f01a0f81ae732687b4122793632c98a9ab3fe
Julia Michel T. <juliamicheltex@gmail.com>
2023-11-03 15:16:13 +0100

    connecting data

--- challenge-js-files/assets/main.js
+++ challenge-js-files/assets/main.js
@@ -6,15 +6,53 @@ document.addEventListener("DOMContentLoaded", function () {
   targetElement.parentNode.insertBefore(canvas, targetElement);
   const ctx = canvas.getContext("2d");
 
+  // let tdElements = document.querySelectorAll("#table1 tbody td:nth-of-type(1)");
+  // let tddata = document.querySelectorAll("#table1 td");
+  // let thElements = document.querySelectorAll(
+  //   "#table1 tbody tr:nth-of-type(1) th"
+  // );
+
   let tdElements = document.querySelectorAll("#table1 tbody td:nth-of-type(1)");
-  let tddata = document.querySelectorAll(
-    "#table1 tbody td:not(:first-of-type)"
+  let tddata = document.querySelectorAll("#table1 td");
+  let thElements = document.querySelectorAll(
+    "#table1 tbody tr:nth-of-type(1) th"
   );
+
+  // let tdElements = row.document.querySelectorAll("#table1 td:nth-of-type(1)");
+  // let tddata = row.cells.document.querySelectorAll("#table1 td");
+  // let thElements = row.document.querySelectorAll(
+  //   "#table1 tbody tr:nth-of-type(1) th"
+  // );
+
+  // console.log("th elements:", thElements[i].textContent, country, data);
+
   let country = [];
-  // let years = [];
+  let years = [];
+  let data = [];
+
+  for (let i = 1; i < thElements.length; i++) {
+    if (thElements[i].textContent > 0) {
+      years.push(thElements[i].textContent, [country], [data]);
+      console.log("la: ", thElements[i].textContent, [country], [data]);
+    }
+
+    // console.log("th elements:", thElements[i].textContent, country, data);
+  }
 
   for (let i = 0; i < tdElements.length; i++) {
     country.push(tdElements[i].textContent);
+
+    for (let j = 1; j < tddata.length; j++) {
+      // console.log("value boucle: ", tddata[i].textContent);
+      let tmp = parseFloat(tddata[j].textContent);
+
+      if (tmp) {
+        data.push(parseFloat(tddata[j].textContent));
+        // console.log(tddata[i].textContent);
+      }
+
+      // console.log("data test :", data);
+    }
   }
 
   let chart = new Chart(ctx, {
@@ -27,10 +65,10 @@ document.addEventListener("DOMContentLoaded", function () {
       // Information about the dataset
       datasets: [
         {
-          label: "Year",
+          label: years,
           backgroundColor: "lightblue",
           borderColor: "royalblue",
-          data: [],
+          data: data,
         },
       ],
     },
@@ -45,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function () {
       },
       title: {
         display: true,
-        text: "Precipitation in Toronto",
+        text: "years",
       },
       scales: {
         yAxes: [
--- work.md
+++ work.md
@@ -6,6 +6,10 @@ tr : body de la rangée
 th : numero de la rangée
 td : chaque element de la rangée
 
+thElements ---> premier element tr du tbody
+creer un filtre th elements pour exclure l'espace vide
+dans th element push un td à la fois de chaque country en excluant le country
+
 creer un div sur js
 creer un canva sur index.html
 
@@ -15,3 +19,7 @@ et donc tu creer une nouvelle table par dessus le tableau de base
 const getid table 1 et table 2
 
 puis tu fais un for pour qu'il lise le tableau et qu'il remette les données à jour
+
+ROW = la colonne
+
+CELL = les cellules
