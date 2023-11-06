8bec20e11543c3fc539ef980850eb35c9e0c0470
Julia Michel T. <juliamicheltex@gmail.com>
2023-11-06 11:54:35 +0100

    dataset generator created

--- challenge-js-files/assets/main.js
+++ challenge-js-files/assets/main.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   let country = [];
   let years = [];
+  let datasets = [];
 
   // Extract years
   for (let i = 1; i < thElements.length; i++) {
@@ -52,6 +53,32 @@ document.addEventListener("DOMContentLoaded", function () {
     countryData.push(countryRow);
   }
 
+  const colors = [
+    "red",
+    "blue",
+    "green",
+    "orange",
+    "purple",
+    "pink",
+    "brown",
+    "gray",
+    "cyan",
+    "magenta",
+    "lime",
+    "black",
+  ];
+
+  // Create separate datasets for each year
+  for (let i = 0; i < years.length; i++) {
+    let dataset = {
+      label: years[i],
+      backgroundColor: colors[i],
+      borderColor: colors[i],
+      data: countryData.map((row) => row[i]), // Extract data for this year
+    };
+    datasets.push(dataset);
+  }
+
   // Print the extracted data for debugging
   console.table("Years:", years);
   console.table("Countries:", country);
@@ -59,20 +86,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
   let chart = new Chart(ctx, {
     // The type of chart we want to create
-    type: "line", // also try bar or other graph types
+    type: "line",
 
     // The data for our dataset
     data: {
       labels: country,
-      // Information about the dataset
-      datasets: [
-        {
-          label: years,
-          backgroundColor: "lightblue",
-          borderColor: "royalblue",
-          data: countryData,
-        },
-      ],
+      datasets: datasets, // Use the separate datasets
     },
 
     // Configuration options
